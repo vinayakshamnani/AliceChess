3bd0f7175e0ea5f75c4166233a5d4c1fb4bb9c22
vkrishnajayanthi <vkrishnajayanthi>
2016-11-04 17:16:52 -0400

    Added random number and a termination logic after 102 moves

--- aliceApp1/src/main/Main.java
+++ aliceApp1/src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 import ai.AliceAI;
@@ -12,6 +13,8 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
         boolean isStarted = true;
         AliceAI ai = AliceAIFactory.GetInstance().getAIComponent();
+        int moveCount = 0;
+        Random rand = new Random();
 
         String readColor = sc.nextLine();
         String player = readColor.equals("you are white") ? "white" : "black";
@@ -25,27 +28,36 @@ public class Main {
 
         while(isStarted){
             String read = sc.nextLine();
-            if(read.substring(6, 11).equals("moves")){
+            if(read.substring(6, 11).equals("moves")) {
+            	moveCount++;
                 //TODO:first check if the move is legal, if it is, update the board
             	ai.update(read);
                 if(player.equals("white")){
                     List<String> whiteMoves = ai.nextWhiteMoves();
                     if(whiteMoves.size() == 0){
-                        System.out.println("white offers draw");
+                    	System.out.println(player + " surrenders");
                         break;
                     }
-                    ai.update(whiteMoves.get(0));
+                    String theMove = whiteMoves.get(rand.nextInt(whiteMoves.size())); 
+                    ai.update(theMove);
 //                    for(String s : whiteMoves) System.out.println(s);
-                    System.out.println(whiteMoves.get(0));
+                    System.out.println(theMove);
                 }
                 else{
                     List<String> blackMoves = ai.nextBlackMoves();
                     if(blackMoves.size() == 0){
-                        System.out.println("black offers draw");
+                    	System.out.println(player + " surrenders");
                         break;
                     }
-                    ai.update(blackMoves.get(0));
-                    System.out.println(blackMoves.get(0));
+                    String theMove = blackMoves.get(rand.nextInt(blackMoves.size()));
+                    ai.update(theMove);
+                    System.out.println(theMove);
+                }
+                
+                if(moveCount >= 102) {
+                	System.out.println(player + " surrenders");
+                	isStarted = false;
+                    break;	
                 }
             }
             else isStarted = false;
